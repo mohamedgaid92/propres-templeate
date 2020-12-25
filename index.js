46eac344019e9c2923299348bf68d40cece6d9ef
mohamedgaid92 <68557033+mohamedgaid92@users.noreply.github.com>
2020-12-25 23:20:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+$(function(){
+
+
+// creating navbar
+
+   $(".navbar form").mouseenter(function(){
+    $(".navbar form  .form-control").css({"display":"block"})
+   });
+   $(".navbar form").mouseleave(function(){
+    $(".navbar form  .form-control").css({"display":"none"})
+   });
+
+
+// creating navbar
+
+
+
+});
