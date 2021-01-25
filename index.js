5f0a18e08da9ccb7c6225b28524089b833948268
mohamedgaid92 <68557033+mohamedgaid92@users.noreply.github.com>
2021-01-25 20:02:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ $(function(){
    $(".navbar form").mouseleave(function(){
     $(".navbar form  .form-control").css({"display":"none"})
    });
-
+   $(".navbar form input").focuse(function(){
+    $(".navbar form  .form-control").css({"display":"block"})
+   });
+   $(".navbar form input").blur(function(){
+    $(".navbar form  .form-control").css({"display":"none"})
+   });
 
 // creating navbar
 
