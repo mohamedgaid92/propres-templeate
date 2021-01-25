2ffacd122af9d22cfe6a3f92ec978baa1fc929ca
mohamedgaid92 <68557033+mohamedgaid92@users.noreply.github.com>
2021-01-25 20:06:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ $(function(){
    $(".navbar form").mouseleave(function(){
     $(".navbar form  .form-control").css({"display":"none"})
    });
-   $(".navbar form input").focuse(function(){
+   $(".navbar form input").focus(function(){
     $(".navbar form  .form-control").css({"display":"block"})
    });
    $(".navbar form input").blur(function(){
